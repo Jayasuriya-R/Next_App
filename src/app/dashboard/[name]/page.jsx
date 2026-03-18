fef24c247df0b4fe559756559e41d11d301fff75
Jayasuriya R <suryaabi3801@gmail.com>
2026-03-18 22:06:04 +0530

    tanstack query for data fetching

--- src/app/dashboard/[name]/page.jsx
+++ src/app/dashboard/[name]/page.jsx
@@ -1,14 +1,25 @@
+'use client';
 
+import { useQuery } from "@tanstack/react-query";
 
-async function page({ params }) {
-   const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${params.name}`);
-  const data = await res.json();
-  console.log(data);
-  return (
-    <div>Dashboard: {data.title}</div>
-  )
+function Page({ params }) {
+
+  const fetchData = async () => {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${params.name}`
+    );
+    return res.json();
+  };
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['todo', params.name], // ✅ important
+    queryFn: fetchData,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return <div>Dashboard: {data.title}</div>;
 }
 
-
-
-export default page
+export default Page;
--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,9 +1,13 @@
+import Providers from "./provider";
+
 const layout = ({children})=>{
     return (
+        <Providers>
         <div>
             <h1>Dashboard Layout</h1>
             {children}
         </div>
+        </Providers>
     )
 }
 export default layout;
